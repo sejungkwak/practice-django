6c757561b2c6872cccc9489d3724ed3b68d75d9f
Sejung Kwak <kwak.sejung@gmail.com>
2022-08-11 15:49:01 +0000

    mod: Modify DATABASES for testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,19 @@ SOCIALACCOUNT_PROVIDERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+DATABASES = {}
 
+if DEBUG is False:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
