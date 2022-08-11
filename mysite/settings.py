cbbf86b2887f2f26c8f008ef55fbcb2394261ede
Sejung Kwak <kwak.sejung@gmail.com>
2022-08-11 20:11:48 +0000

    fix: Correct allowed_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://django-postgresql.herokuapp.com',
+    'django-postgresql.herokuapp.com',
     'localhost',
 ]
 
